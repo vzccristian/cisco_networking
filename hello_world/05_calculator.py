5813f226f5d500637a36d865e072289d1319ed04
Cristian Vázquez <vzccristian@gmail.com>
2019-03-27 17:07:02 +0100

    Forma pythonica

--- hello_world/05_calculator.py
+++ hello_world/05_calculator.py
@@ -1,12 +1,21 @@
+operations = {"+": lambda x, y: x + y,
+              "-": lambda x, y: x - y,
+              "*": lambda x, y: x * y,
+              "/": lambda x, y: x / y,
+              "e": lambda x, y: x ** y,
+              }
+
 try:
     a = int(input("First number: "))
     b = int(input("Second number: "))
-    x = input("Operation (+,*,/,e): ")
-
-    if x == "+": print(a+b)
-    elif x == "*": print(a*b)
-    elif x == "/": print(a/b)
-    elif x == "e": print(a**b)
-    else: print("Invalid operator.")
+    x = input("Operation (+,-,*,/,e): ")
+    print(operations[x](a, b)) if (x in operations.keys()) else print("Invalid operator.")
 except Exception as e:
     print(e)
+
+# Test
+assert 5+4 == operations["+"](5, 4)
+assert 5-4 == operations["-"](5, 4)
+assert 5*4 == operations["*"](5, 4)
+assert 5/4 == operations["/"](5, 4)
+assert 5**4 == operations["e"](5, 4)
