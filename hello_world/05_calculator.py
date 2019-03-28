0dac731b49a610d7180645d9c22eb4ebbe2daf73
Cristian Vázquez <vzccristian@gmail.com>
2019-03-28 17:09:28 +0100

    Versión mejorada

--- hello_world/05_calculator.py
+++ hello_world/05_calculator.py
@@ -5,17 +5,39 @@ operations = {"+": lambda x, y: x + y,
               "e": lambda x, y: x ** y,
               }
 
-try:
-    a = int(input("First number: "))
-    b = int(input("Second number: "))
-    x = input("Operation (+,-,*,/,e): ")
-    print(operations[x](a, b)) if (x in operations.keys()) else print("Invalid operator.")
-except Exception as e:
-    print(e)
+def calculator(a, b, x):
+    try:
+        return (operations[x](a, b)) if (x in operations.keys() or x in len(list(operations.keys()))) else print(
+            "Invalid operator.")
+    except TypeError as e:
+        x = int(x)
+        if x > -1 and x < len(list(operations.keys())):
+            return (operations[list(operations.keys())[x]](a, b))
+        else:
+            return ("Invalid operator.")
+    except Exception as e:
+        return ("Invalid operator.")
 
 # Test
-assert 5+4 == operations["+"](5, 4)
-assert 5-4 == operations["-"](5, 4)
-assert 5*4 == operations["*"](5, 4)
-assert 5/4 == operations["/"](5, 4)
-assert 5**4 == operations["e"](5, 4)
+assert 5 + 4 == calculator(5, 4, "+")
+assert 5 - 4 == calculator(5, 4, "-")
+assert 5 * 4 == calculator(5, 4, "*")
+assert 5 / 4 == calculator(5, 4, "/")
+assert 5 ** 4 == calculator(5, 4, "e")
+
+assert 5 + 4 == calculator(5, 4, 0)
+assert 5 - 4 == calculator(5, 4, 1)
+assert 5 * 4 == calculator(5, 4, 2)
+assert 5 / 4 == calculator(5, 4, 3)
+assert 5 ** 4 == calculator(5, 4, 4)
+
+
+# Main
+if __name__ == '__main__':
+    try:
+        a = int(input("First number: "))
+        b = int(input("Second number: "))
+        x = input("Operation (+,-,*,/,e): ")
+        print(calculator(a, b, x))
+    except Exception as e:
+        print(e)
